57af100e14de95761362c9214da06554a7fdc243
k20059052 <101188329+k20059052@users.noreply.github.com>
2022-09-30 03:33:32 +0100

    employee solutions

--- employee.py
+++ employee.py
@@ -6,26 +6,108 @@ class Employee:
         self.name = name
 
     def get_pay(self):
-        pass
+        print(self.name)
 
     def __str__(self):
         return self.name
 
+class EmployeeSalaryContractNoCommission(Employee):
+    def __init__(self,name,salary):
+        super().__init__(name)
+        self.salary = salary
+
+    def get_pay(self):
+        return self.salary
+
+    def __str__(self):
+        return f"{self.name} works on a monthly salary of {self.salary}.  Their total pay is {self.salary}."
+
+class EmployeeHourlyContractNoCommission(Employee):
+    def __init__(self,name,hours,rate):
+        super().__init__(name)
+        self.hours = hours
+        self.rate = rate
+    def get_pay(self):
+        return self.hours * self.rate
+
+    def __str__(self):
+        return f'{self.name} works on a contract of {self.hours} hours at {self.rate}/hour.  Their total pay is {self.hours * self.rate}.'
+
+
+class EmployeeSalaryContractBonus(Employee):
+    def __init__(self,name,salary,bonus):
+        super().__init__(name)
+        self.salary = salary
+        self.bonus = bonus
+
+    def get_pay(self):
+        return self.salary + self.bonus
+
+    def __str__(self):
+        return f"{self.name} works on a monthly salary of {self.salary} and receives a bonus commission of {self.bonus}. Their total pay is {self.bonus + self.salary}."
+
+class HourlyContractBonusCommission(Employee):
+    def __init__(self,name,hours,rate,bonus):
+        super().__init__(name)
+        self.hours = hours
+        self.rate = rate
+        self.bonus = bonus
+
+    def get_pay(self):
+        return (self.hours * self.rate) + self.bonus
+
+    def __str__(self):
+        return f"{self.name} works on a contract of {self.hours} hours at {self.rate}/hour and receives a bonus commission of {self.bonus}. Their total pay is {(self.hours * self.rate) + self.bonus}."
+
+
+class HourlyContractContractCommission(Employee):
+    def __init__(self, name, hours, rate,ncontracts,cpc):
+        super().__init__(name)
+        self.hours = hours
+        self.rate = rate
+        self.ncontracts = ncontracts
+        self.cpc = cpc
+
+    def get_pay(self):
+        return (self.hours * self.rate) + (self.ncontracts * self.cpc)
+
+    def __str__(self):
+        return f"{self.name} works on a contract of {self.hours} hours at {self.rate}/hour and receives a commission for {self.ncontracts} contract(s) at {self.cpc}/contract. Their total pay is {(self.hours * self.rate) + (self.ncontracts * self.cpc)}."
+
+class SalaryContractContractCommission(Employee):
+    def __init__(self,name,salary,ncontracts,cpc):
+        super().__init__(name)
+        self.salary = salary
+        self.ncontracts = ncontracts
+        self.cpc = cpc
+
+    def get_pay(self):
+        return self.salary + (self.ncontracts * self.cpc)
+
+    def __str__(self):
+        return f"{self.name} works on a monthly salary of {self.salary} and receives a commission for {self.ncontracts} contract(s) at {self.cpc}/contract. Their total pay is {self.salary + (self.ncontracts * self.cpc)}."
+
+
+
+
+
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
-billie = Employee('Billie')
+billie = EmployeeSalaryContractNoCommission("Billie","4000")
 
-# Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = Employee('Charlie')
 
-# Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = Employee('Renee')
+charlie = EmployeeHourlyContractNoCommission("Charlie",100,25)
 
-# Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = Employee('Jan')
 
-# Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = Employee('Robbie')
+robbie = EmployeeSalaryContractBonus("Robbie",2000,1500)
 
-# Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = Employee('Ariel')
+
+ariel = HourlyContractBonusCommission("Ariel",120,30,600)
+
+
+jan = HourlyContractContractCommission("Jan",150,25,3,220)
+
+
+renee = SalaryContractContractCommission("Renee",3000,4,200)
+
+
